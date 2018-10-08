5fef72dac6f9934c6f1161f05ed933893d8522c8
berli <libo22@gmail.com>
2018-10-08 20:43:45 +0800

    new file:   data/VOC0712/labelmap_voc.prototxt 	modified:   run.sh 	new file:   run300.sh 	new file:   run300coco.sh

--- build/examples/ssd/run300coco.sh
+++ build/examples/ssd/run300coco.sh
@@ -5,5 +5,5 @@
 # Created Time: 2018年10月08日 星期一 19时58分54秒
 #########################################################################
 #!/bin/bash
-./ssd_detect   models_VOC0712_SSD_512x512/VGGNet/VOC0712/SSD_512x512/deploy.prototxt models_VOC0712_SSD_512x512/VGGNet/VOC0712/SSD_512x512/VGG_VOC0712_SSD_512x512_iter_120000.caffemodel --confidence_threshold 0.3 test.txt
+./ssd_detect   models/VGGNet/VOC0712/SSD_300x300_coco/deploy.prototxt models/VGGNet/VOC0712/SSD_300x300_coco/VGG_coco_SSD_300x300.caffemodel --confidence_threshold 0.3 test.txt
 
