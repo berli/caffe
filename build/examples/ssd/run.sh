ed16f8874cb1d54318533b8626d551ae24e85ae1
berli <libo22@gmail.com>
2018-10-08 19:59:47 +0800

    modified:   ../../../CMakeLists.txt 	renamed:    ../../../examples/ssd/201704051756.jpg -> 201704051756.jpg 	new file:   run.sh 	new file:   test.txt

---
+++ build/examples/ssd/run.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: run.sh
+# Author: berli
+# mail: libo_5@163.com
+# Created Time: 2018年10月08日 星期一 19时58分54秒
+#########################################################################
+#!/bin/bash
+./ssd_detect   models/VGGNet/VOC0712/SSD_512x512/deploy.prototxt models/VGGNet/VOC0712/SSD_512x512/VGG_VOC0712_SSD_512x512_iter_120000.caffemodel --confidence_threshold 0.4 test.txt
+
